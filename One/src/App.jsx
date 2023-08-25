e5ecb909db010bf925ada4f999d98dae2ee19e8c
Rajarshi Sarkar <rajsarkar799@gmail.com>
2023-08-25 19:24:37 +0530

    One

---
+++ One/src/App.jsx
@@ -0,0 +1,9 @@
+import PracticeOne from "./PracticeOne"
+
+function App() {
+  return (
+    <div><PracticeOne/></div>
+  )
+}
+
+export default App
---
+++ One/src/PracticeOne.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function PracticeOne() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Simple React Counter App</h1>
+      <div className="counter">
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <button onClick={increment}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default PracticeOne;
